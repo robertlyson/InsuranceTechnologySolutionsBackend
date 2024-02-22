cbf5819c4edb19006b0142228767a71f3665355f
Robert Łysoń <robertlyson@gmail.com>
2024-02-22 17:45:41 +0100

    testcontainers too slow, will test with emulator

--- Claims.Tests/GlobalFixture.cs
+++ Claims.Tests/GlobalFixture.cs
@@ -1,39 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using NUnit.Framework;
-using Testcontainers.CosmosDb;
 
 namespace Claims.Tests;
 
 [SetUpFixture]
 public class ApplicationFixture
 {
-    private CosmosDbContainer? _cosmosDbContainer;
     protected WebApplicationFactory<Program>? Factory { get; private set; }
 
     [OneTimeSetUp]
     public async Task RunBeforeAnyTests()
     {
-        _cosmosDbContainer = new CosmosDbBuilder()
-            .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest")
-            .WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "1")
-            .WithEnvironment("AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE", "127.0.0.1")
-            .WithCleanUp(true)
-            .Build();
-        await _cosmosDbContainer.StartAsync();
-
-        var cosmosDbConnectionString = _cosmosDbContainer.GetConnectionString();
-
         Factory = new WebApplicationFactory<Program>()
             .WithWebHostBuilder(builder =>
             {
-                builder.UseSetting("CosmosDb:ConnectionString", cosmosDbConnectionString);
+                builder.UseSetting("CosmosDb:ConnectionString", "");
             });
     }
 
     [OneTimeTearDown]
     public async Task RunAfterAnyTests()
     {
-        await _cosmosDbContainer!.DisposeAsync();
         await Factory!.DisposeAsync();
     }
 }
